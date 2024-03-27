ed67086b8aaa6db1359c0501273bf118fe78f19c
Prerna <shahiprerna94@gmail.com>
2024-03-27 18:54:14 +1100

    More commenting

--- A Star Search.py
+++ A Star Search.py
@@ -23,32 +23,71 @@ import math
 import heapq
 
 class Problem(object):
+
+    """The abstract class for a formal problem. You should subclass
+    this and implement the methods actions and result, and possibly
+    __init__, goal_test, and path_cost. Then you will create instances
+    of your subclass and solve them with the various search functions."""
+
     def __init__(self, initial, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal. Your subclass's constructor can add
+        other arguments."""
         self.initial = initial
         self.goal = goal
     def is_in(self,elt, seq):
+        """Similar to (elt in seq), but compares with 'is', not '=='."""
         return any(x is elt for x in seq)
 
     def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
         raise NotImplementedError
 
-    def result(self, state, action):
+    def result(self, state, action):#transition model
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
         raise NotImplementedError
 
     def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
         if isinstance(self.goal, list):
             return self.is_in(state, self.goal)
         else:
             return state == self.goal
 
     def path_cost(self, c, state1, action, state2):
-        return c + 1
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2.  If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+        return c + 1 #uniform cost
 
     def value(self, state):
+        """For optimization problems, each state has a value.  Hill-climbing
+        and related algorithms try to maximize this value."""
         raise NotImplementedError
 
 class Node:
+
+    """A node in a search tree. Contains a pointer to the parent (the node
+    that this is a successor of) and to the actual state for this node. Note
+    that if a state is arrived at by two paths, then there are two nodes with
+    the same state.  Also includes the action that got us to this state, and
+    the total path_cost (also known as g) to reach the node.  Other functions
+    may add an f and h value; see best_first_graph_search and astar_search for
+    an explanation of how the f and h values are handled. You will not need to
+    subclass this class."""
+
     def __init__(self, state, parent=None, action=None, path_cost=0):
+        """Create a search tree Node, derived from a parent by an action."""
         self.state = state
         self.parent = parent
         self.action = action
@@ -63,11 +102,12 @@ class Node:
     def __lt__(self, node):
         return self.state < node.state
 
-    def expand(self, problem):
+    def expand(self, problem):#expansion
+        """List the nodes reachable in one step from this node."""
         return [self.child_node(problem, action)
                 for action in problem.actions(self.state)]
 
-    def child_node(self, problem, action):
+    def child_node(self, problem, action):#generation
         next_state = problem.result(self.state, action)
         next_node = Node(next_state, self, action,
                     problem.path_cost(self.path_cost, self.state,
@@ -75,19 +115,22 @@ class Node:
         return next_node
 
     def solution(self):
+        """Return the sequence of actions to go from the root to this node."""
         return [node.action for node in self.path()[1:]]
 
     def path(self):
+        """Return a list of nodes forming the path from the root to this node."""
         node, path_back = self, []
         while node:
             path_back.append(node)
             node = node.parent
         return list(reversed(path_back))
+
     def __eq__(self, other):
         return isinstance(other, Node) and self.state == other.state
 
     def __hash__(self):
-        return hash(self.state)
+        return hash(self.state)#quickly find the infor/comparison
 
 class GraphProblem(Problem):
     def __init__(self, initial, goal, graph):
@@ -127,26 +170,46 @@ class GraphProblem(Problem):
             return self.infinity
 
 class Graph:
+    """A graph connects nodes (vertices) by edges (links). Each edge can also
+    have a length associated with it. The constructor call is something like:
+        g = Graph({'A': {'B': 1, 'C': 2})
+    this makes a graph with 3 nodes, A, B, and C, with an edge of length 1 from
+    A to B,  and an edge of length 2 from A to C. You can also do:
+        g = Graph({'A': {'B': 1, 'C': 2}, directed=False)
+    This makes an undirected graph, so inverse links are also added. The graph
+    stays undirected; if you add more links with g.connect('B', 'C', 3), then
+    inverse link is also added. You can use g.nodes() to get a list of nodes,
+    g.get('A') to get a dict of links out of A, and g.get('A', 'B') to get the
+    length of the link from A to B. 'Lengths' can actually be any object at
+    all, and nodes can be any hashable object."""
+
     def __init__(self, graph_dict=None, directed=True):
-        self.graph_dict = graph_dict or {}
+        self.graph_dict = graph_dict or {} #ensures that graph_dict will always be a dictionary, even if it is undefined or empty.
         self.directed = directed
         if not directed:
             self.make_undirected()
 
     def make_undirected(self):
+        """Make a digraph into an undirected graph by adding symmetric edges."""
         for a in list(self.graph_dict.keys()):
             for (b, dist) in self.graph_dict[a].items():
                 self.connect1(b, a, dist)
 
     def connect(self, A, B, distance=1):
+        """Add a link from A and B of given distance, and also add the inverse
+        link if the graph is undirected."""
         self.connect1(A, B, distance)
         if not self.directed:
             self.connect1(B, A, distance)
 
     def connect1(self, A, B, distance):
+        """Add a link from A to B of given distance, in one direction only."""
         self.graph_dict.setdefault(A, {})[B] = distance
 
     def get(self, a, b=None):
+        """Return a link distance or a dict of {node: distance} entries.
+        .get(a,b) returns the distance or None;
+        .get(a) returns a dict of {node: distance} entries, possibly {}."""
         links = self.graph_dict.setdefault(a, {})
         if b is None:
             return links
@@ -154,6 +217,7 @@ class Graph:
             return links.get(b)
 
     def nodes(self):
+        """Return a list of nodes in the graph."""
         s1 = set([k for k in self.graph_dict.keys()])
         s2 = set([k2 for v in self.graph_dict.values() for k2, v2 in v.items()])
         nodes = s1.union(s2)
@@ -161,9 +225,10 @@ class Graph:
 
 
 def UndirectedGraph(graph_dict=None):
+    """Build a Graph where every edge (including future ones) goes both ways."""
     return Graph(graph_dict=graph_dict, directed=False)
 
-# Define the Melbourne map as an undirected graph with distances in km
+# Define the local map as an undirected graph with distances in km
 local_map = UndirectedGraph(dict(
     Royal_Gardens=dict(Roudabout_2=56),
     Cranbourne_Park_Shopping_Centre=dict(Roudabout_2=29, Public_Toilet_2 = 16),
@@ -185,7 +250,7 @@ local_map = UndirectedGraph(dict(
 ))
 
 
-# Define the locations of each suburb in Melbourne
+# Define the locations of each suburb in the locality
 local_map.locations = dict(
     Royal_Gardens=(145.2691829, -36.7304496),
     Cranbourne_Park_Shopping_Centre=(145.2841866, -36.1089868),
@@ -210,7 +275,7 @@ local_map.locations = dict(
     Shopping_Center_Parking =(145.28152, -38.11188)
 )
 
-# Define the Melbourne problem
+# Define the local area problem
 local_problem = GraphProblem('Cranbourne_Park_Shopping_Centre', 'Royal_Gardens', local_map)
 
 local_locations = local_map.locations
@@ -237,7 +302,7 @@ def show_map(graph_data, node_colors=None):
     edge_weights = graph_data['edge_weights']
 
     # set the size of the plot
-    plt.figure(figsize=(10, 7))
+    plt.figure(figsize=(20, 7))
     # draw the graph (both nodes and edges) with locations
     nx.draw(G, pos={k: node_positions[k] for k in G.nodes()},
             node_color=[node_colors[node] for node in G.nodes()], linewidths=0.3, edgecolors='k',with_labels = True)
@@ -260,9 +325,6 @@ def show_map(graph_data, node_colors=None):
 
 show_map(local_graph_data)
 
-def euclidean_distance(self, a, b):
-  return math.dist(a, b)
-
 def final_path_colors(initial_node_colors, problem, solution):
     "Return a node_colors dict of the final path provided the problem and solution."
 
@@ -276,79 +338,46 @@ def final_path_colors(initial_node_colors, problem, solution):
 
 def display_visual(graph_data, user_input, algorithm=None, problem=None):
     initial_node_colors = graph_data['node_colors']
-    if user_input is False:
-        def slider_callback(iteration):
-            # don't show graph for the first time running the cell calling this function
-            try:
-                show_map(graph_data, node_colors=all_node_colors[iteration])
-            except:
-                pass
+    node_colors = dict(initial_node_colors)
+    def slider_callback(iteration):
+        # don't show graph for the first time running the cell calling this function
+        try:
+            show_map(graph_data, node_colors=all_node_colors[iteration])
+        except:
+            pass
 
-        def visualize_callback(visualize):
-            if visualize is True:
-                button.value = False
+    def visualize_callback(visualize):
+        if visualize is True:
+            button.value = False
 
-                global all_node_colors
+            problem = GraphProblem(start_dropdown.value, end_dropdown.value, local_map)
+            global all_node_colors
 
-                iterations, all_node_colors, node = algorithm(problem)
-                solution = node.solution()
-                all_node_colors.append(final_path_colors(all_node_colors[0], problem, solution))
+            iterations, all_node_colors, node = algorithm(problem)
+            solution = node.solution()
+            all_node_colors.append(final_path_colors(all_node_colors[0], problem, solution))
 
-                slider.max = len(all_node_colors) - 1
+            slider.max = len(all_node_colors) - 1
 
-                for i in range(slider.max + 1):
-                    slider.value = i
-                    time.sleep(.5)
+            for i in range(slider.max + 1):
+                slider.value = i
+                time.sleep(.5)
 
-        slider = widgets.IntSlider(min=0, max=1, step=1, value=0)
-        slider_visual = widgets.interactive(slider_callback, iteration=slider)
-        display(slider_visual)
+    start_dropdown = widgets.Dropdown(description="Start location: ",
+                                      options=sorted(list(node_colors.keys())), value="Cranbourne_Park_Shopping_Centre")
+    display(start_dropdown)
 
-        button = widgets.ToggleButton(value=False)
-        button_visual = widgets.interactive(visualize_callback, visualize=button)
-        display(button_visual)
+    end_dropdown = widgets.Dropdown(description="Goal location: ",
+                                    options=sorted(list(node_colors.keys())), value="Royal_Gardens")
+    display(end_dropdown)
 
-    if user_input is True:
-        node_colors = dict(initial_node_colors)
-        def slider_callback(iteration):
-            # don't show graph for the first time running the cell calling this function
-            try:
-                show_map(graph_data, node_colors=all_node_colors[iteration])
-            except:
-                pass
+    button = widgets.ToggleButton(value=False)
+    button_visual = widgets.interactive(visualize_callback, visualize=button)
+    display(button_visual)
 
-        def visualize_callback(visualize):
-            if visualize is True:
-                button.value = False
-
-                problem = GraphProblem(start_dropdown.value, end_dropdown.value, local_map)
-                global all_node_colors
-
-                iterations, all_node_colors, node = algorithm(problem)
-                solution = node.solution()
-                all_node_colors.append(final_path_colors(all_node_colors[0], problem, solution))
-
-                slider.max = len(all_node_colors) - 1
-
-                for i in range(slider.max + 1):
-                    slider.value = i
-                    time.sleep(.5)
-
-        start_dropdown = widgets.Dropdown(description="Start location: ",
-                                          options=sorted(list(node_colors.keys())), value="Cranbourne_Park_Shopping_Centre")
-        display(start_dropdown)
-
-        end_dropdown = widgets.Dropdown(description="Goal location: ",
-                                        options=sorted(list(node_colors.keys())), value="Royal_Gardens")
-        display(end_dropdown)
-
-        button = widgets.ToggleButton(value=False)
-        button_visual = widgets.interactive(visualize_callback, visualize=button)
-        display(button_visual)
-
-        slider = widgets.IntSlider(min=0, max=1, step=1, value=0)
-        slider_visual = widgets.interactive(slider_callback, iteration=slider)
-        display(slider_visual)
+    slider = widgets.IntSlider(min=0, max=1, step=1, value=0)
+    slider_visual = widgets.interactive(slider_callback, iteration=slider)
+    display(slider_visual)
 
 import functools
 
